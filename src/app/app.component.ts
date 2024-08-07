c76989d96f7506b2295faa4a29e25990508a7fbf
oleh <oleh.abanin@yolo.com>
2024-08-07 14:51:32 +0300

    fix: add service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,48 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NewAccountComponent} from "./new-account/new-account.component";
 import {AccountComponent} from "./account/account.component";
 import {NgForOf} from "@angular/common";
+
+import {AccountsService} from "./accounts.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, NewAccountComponent, AccountComponent, NgForOf],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers:[AccountsService]
 })
-export class AppComponent {
-  accounts = [
-    {
-      name: 'Master Account',
-      status: 'active'
-    },
-    {
-      name: 'Testaccount',
-      status: 'inactive'
-    },
-    {
-      name: 'Hidden Account',
-      status: 'unknown'
-    }
-  ];
-
-  onAccountAdded(event: {name: string, status: string}) {
-    this.accounts.push(event);
+export class AppComponent implements OnInit{
+  accounts: {name:string, status:string}[] = [];
+  constructor(private accountsService:AccountsService)  {
   }
 
-  onStatusChanged(updateInfo: {id: number, newStatus: string}) {
-    this.accounts[updateInfo.id].status = updateInfo.newStatus;
+  ngOnInit() {
+    this.accounts = this.accountsService.accounts
   }
+
+  // accounts = [
+  //   {
+  //     name: 'Master Account',
+  //     status: 'active'
+  //   },
+  //   {
+  //     name: 'Testaccount',
+  //     status: 'inactive'
+  //   },
+  //   {
+  //     name: 'Hidden Account',
+  //     status: 'unknown'
+  //   }
+  // ];
+  //
+  // onAccountAdded(event: {name: string, status: string}) {
+  //   this.accounts.push(event);
+  // }
+  //
+  // onStatusChanged(updateInfo: {id: number, newStatus: string}) {
+  //   this.accounts[updateInfo.id].status = updateInfo.newStatus;
+  // }
 }
