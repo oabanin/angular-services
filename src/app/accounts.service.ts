9e39ccc4d0efd411c08c177a1e0378cd94cf618b
oleh <oleh.abanin@yolo.com>
2024-08-07 17:45:47 +0300

    fix: service

--- src/app/accounts.service.ts
+++ src/app/accounts.service.ts
@@ -1,7 +1,7 @@
 import {LoggingService} from "./logging.service";
 import {Injectable} from "@angular/core";
 
-@Injectable({providedIn:"root"} )
+@Injectable( )
 
 export class AccountsService {
   accounts = [
